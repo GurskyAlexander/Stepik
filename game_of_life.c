d16af1c6b38cd5cfb28c6ba19a63703a00cc1f82
GurskyAlexander <148016452+GurskyAlexander@users.noreply.github.com>
2025-02-16 19:10:03 +1100

    final

--- game_of_life.c
+++ game_of_life.c
@@ -1,149 +1,151 @@
-
+#include <ncurses.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-// Устанавливает позицию курсора в терминале
-void setCursorPos(int x, int y) {
-    printf("\033[%d;%dH", y, x);
-}
-
-// Рисует поле с границами
-void draw_board(int height, int width) {
-    // Выделяем память для поля
-    char **field = (char **)malloc(height * sizeof(char *));
-    for (int i = 0; i < height; i++) {
-        field[i] = (char *)malloc(width * sizeof(char));
-    }
-
-    // Заполняем поле
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            if (y == 0 || y == height - 1) {
-                // Верхняя и нижняя границы
-                field[y][x] = '-';
-            } else if (x == 0 || x == width - 1) {
-                // Левая и правая границы
-                field[y][x] = '|';
-            } else {
-                // Внутренняя часть поля
-                field[y][x] = ' ';
-            }
-        }
-    }
-
-    // Выводим поле на экран
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            printf("%c", field[y][x]);
-        }
-        printf("\n");
-    }
-
-    // Освобождаем память
-    for (int i = 0; i < height; i++) {
-        free(field[i]);
-    }
-    free(field);
-}
-
-void draw_life(int a, int b, int**matrix){
-    for (int y = 0; y < b; y++) {
-        for (int x = 0; x < a; x++) {
-            printf("%d", matrix[y][x]); // Выводим числа
-            
-        }
-        setCursorPos(2, y+3);
-    }
-}
-
-int neighbour_sum(int** matrix, int i, int a, int x, int b){
-    int sum = 0;
-
-    int prev_row = (i == 0) ? b - 1 : i - 1; // Сосед сверху
-    int next_row = (i == b - 1) ? 0 : i + 1; // Сосед снизу
-    int prev_col = (x == 0) ? a - 1 : x - 1; // Сосед слева
-    int next_col = (x == a - 1) ? 0 : x + 1; // Сосед справа
-
-    sum += matrix[prev_row][prev_col]; // Верхний левый
-    sum += matrix[prev_row][x];        // Верхний
-    sum += matrix[prev_row][next_col]; // Верхний правый
-    sum += matrix[i][prev_col];        // Левый
-    sum += matrix[i][next_col];        // Правый
-    sum += matrix[next_row][prev_col]; // Нижний левый
-    sum += matrix[next_row][x];        // Нижний
-    sum += matrix[next_row][next_col]; // Нижний правый
-
-    return 0;
-}
-
-void change_life(int**matrix, int a, int b){
-    int **matrix_b = (int **)calloc(b, sizeof(int *));
-    for (int i = 0; i < b; i++) {
-        matrix_b[i] = (int *)calloc(a, sizeof(int));
-        for (int x = 0; x < b; x++) {
-            matrix_b[i][x] = matrix[i][x];
-        }
-    }
-
-    for (int i = 0; i < b; i++) {
-        for (int x = 0; x < b; x++) {
-            if (matrix_b[i][x]==0){
-                if(neighbour_sum(matrix, i, a, x, b) == 3){
-                    matrix[i][x] = 1;
-                }
-            }
-            else{
-                if(neighbour_sum(matrix, i, a, x, b) <2){
-                    matrix[i][x] = 0;
-                }
-                else if(neighbour_sum(matrix, i, a, x, b) >3){
-                    matrix[i][x] = 0;
-                }
-
-            }
-        }
-    }
-
-
-    
-    for (int i = 0; i < b; i++) {
-        free(matrix_b[i]);
-    }
-    free(matrix_b);
-
-}
+void draw_life(int a, int b, int **matrix);
+int neighbour_sum(int **matrix, int current_row, int columns, int current_column, int rows);
+void change_life(int **matrix, int a, int b);
+void read_from_file(const char *filename, int **matrix);
+void greeting(int **matrix);
 
 int main() {
-
-    int a = 80; // Ширина поля
-    int b = 25; // Высота поля
-    int sleep_val = 1000;
+    int a = 80;
+    int b = 25;
+    int sleep_val = 512;
 
     int **matrix = (int **)calloc(b, sizeof(int *));
     for (int i = 0; i < b; i++) {
         matrix[i] = (int *)calloc(a, sizeof(int));
-
+        for (int x = 0; x < a; x++) matrix[i][x] = 0;
     }
 
-    printf("\33[0d\33[2J");
-    setCursorPos(0, 1);
-    draw_board(b+2, a+2);
-    while(1){    
-        setCursorPos(2, 2);
+    greeting(matrix);
+    // Инициализация ncurses
+    initscr();
+    // Включение неблокирующего режима
+    nodelay(stdscr, TRUE);
+
+    while (1) {
         draw_life(a, b, matrix);
         change_life(matrix, a, b);
-        // sleep(sleep_val);
-        setCursorPos(1,30);
+        refresh();
+
+        char ch = getch();
+        if (ch == 'q' || ch == 'Q') break;
+        if (ch == '+') sleep_val = (sleep_val > 100) ? sleep_val / 2 : 100;
+        if (ch == '-') sleep_val *= 2;
+        // Неблокирующая задержка
+        napms(sleep_val);
     }
-    
 
-    
-
-    
-    for (int i = 0; i < b; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
-
+    endwin();
     return 0;
+}
+
+void draw_life(int a, int b, int **matrix) {
+    for (int y = 0; y < b; y++) {
+        for (int x = 0; x < a; x++) mvaddch(y, x, matrix[y][x] ? '@' : '.');
+    }
+}
+
+int neighbour_sum(int **matrix, int current_row, int columns, int current_column, int rows) {
+    int sum = 0;
+    int prev_row = (current_row - 1 + rows) % rows;
+    int next_row = (current_row + 1) % rows;
+    int prev_col = (current_column - 1 + columns) % columns;
+    int next_col = (current_column + 1) % columns;
+
+    sum += matrix[prev_row][prev_col];
+    sum += matrix[prev_row][current_column];
+    sum += matrix[prev_row][next_col];
+    sum += matrix[current_row][prev_col];
+    sum += matrix[current_row][next_col];
+    sum += matrix[next_row][prev_col];
+    sum += matrix[next_row][current_column];
+    sum += matrix[next_row][next_col];
+
+    return sum;
+}
+
+void change_life(int **matrix, int a, int b) {
+    int **matrix_b = (int **)calloc(b, sizeof(int *));
+    for (int i = 0; i < b; i++) {
+        matrix_b[i] = (int *)calloc(a, sizeof(int));
+        for (int x = 0; x < a; x++) matrix_b[i][x] = matrix[i][x];
+    }
+
+    for (int i = 0; i < b; i++) {
+        for (int x = 0; x < a; x++) {
+            if (matrix_b[i][x] == 0) {
+                if (neighbour_sum(matrix_b, i, a, x, b) == 3) matrix[i][x] = 1;
+            } else {
+                if (neighbour_sum(matrix_b, i, a, x, b) < 2)
+                    matrix[i][x] = 0;
+                else if (neighbour_sum(matrix_b, i, a, x, b) > 3)
+                    matrix[i][x] = 0;
+            }
+        }
+    }
+
+    for (int i = 0; i < b; i++) free(matrix_b[i]);
+
+    free(matrix_b);
+}
+
+void read_from_file(const char *filename, int **matrix) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Ошибка: не удалось открыть файл %s\n", filename);
+        return;
+    }
+
+    int x, y;
+    while (fscanf(file, "%d %d", &x, &y) == 2) {
+        if (x >= 0 && x < 80 && y >= 0 && y < 25)
+            // Устанавливаем клетку как живую
+            matrix[y][x] = 1;
+        else
+            return;
+    }
+
+    fclose(file);
+}
+
+void greeting(int **matrix) {
+    char str2[100];
+    while (1) {
+        int val;
+        int coord_x;
+        int coord_y;
+
+        printf("Добро пожаловать в игру жизнь.\n");
+        printf(" Изменение скорости + или -, выход из игры - q.\n");
+        printf("Как хотите вводить начальное состояние:\n");
+        printf("    1. Файл\n");
+        printf("    2. Консоль\n");
+
+        if (scanf("%d", &val)) {
+            if (val == 1) {
+                printf(" \nВведите название файла: ");
+                if (scanf("%99s", str2)) {
+                    read_from_file(str2, matrix);
+                    return;
+                }
+            } else if (val == 2) {
+                printf(" \nВведите координаты в консоль(введите некорректное значение для остановки): ");
+                while (1) {
+                    if (scanf("%d", &coord_x) && scanf("%d", &coord_y) && coord_x >= 0 && coord_x <= 80 &&
+                        coord_y >= 0 && coord_y <= 25)
+                        matrix[coord_y][coord_x] = 1;
+                    else
+                        return;
+                }
+            }
+        } else
+            while (getchar() != '\n');
+
+        clear();
+    }
 }
